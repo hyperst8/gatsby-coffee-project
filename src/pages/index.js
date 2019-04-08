e948a131c68e8fbe7538364be4cdd04429412a45
hyperst8 <Huy.Nguyen@asker.kommune.no>
2019-04-08 15:44:04 +0200

    Menu component initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,49 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React from 'react';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import BackgroundSection from "../components/Globals/BackgroundSection"
-import Info from "../components/Home/Info"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import BackgroundSection from '../components/Globals/BackgroundSection';
+import Info from '../components/Home/Info';
+import Menu from '../components/Home/Menu';
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <BackgroundSection
-      img={data.img.childImageSharp.fluid}
-      title="Regular Joe's"
-      styleClass="default-background"
-    />
-    <Info />
-  </Layout>
-)
+	<Layout>
+		<SEO title="Home" keywords={[ `gatsby`, `application`, `react` ]} />
+		<BackgroundSection img={data.img.childImageSharp.fluid} title="Regular Joe's" styleClass="default-background" />
+		<Info />
+		<Menu items={data.menu} />
+	</Layout>
+);
 
 export const query = graphql`
-  {
-    img: file(relativePath: { eq: "default-background.jpeg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`
+	{
+		img: file(relativePath: { eq: "default-background.jpeg" }) {
+			childImageSharp {
+				fluid {
+					...GatsbyImageSharpFluid_tracedSVG
+				}
+			}
+		}
+		menu: allContentfulCoffeeItem {
+			edges {
+				node {
+					id
+					title
+					description {
+						description
+					}
+					price
+					category
+					image {
+						fixed(width: 50, height: 50) {
+							...GatsbyContentfulFixed_tracedSVG
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 
-export default IndexPage
+export default IndexPage;
